3a563d87255e1df51b19bb48aa99181f07ba3a9c
Noah Wyche <noahwyche1@gmail.com>
2023-10-24 18:43:25 -0400

    In this commit I have updated the id names for the hour blocks so that they now follow the 24 hour default clock and function as intended dynamically shifting the class between past, present, and future and thus the styles that go along with them.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,19 @@
-//linking Day.js to keep live track of the current date and timel
+$(document).ready(function () {
+    // Displays the current date at the top of the page
+    const currentDay = dayjs();
+    $('#currentDay').text(currentDay.format('MMM D, YYYY, h:mm'));
 
+    // Update time block classes
+    updateTimeBlockClasses();
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // Displays current date and time at the top of the page
-    const currentDay = dayjs()
-    $('#currentDay').text(currentDay.format('MMM D, YYYY, h:mm')); // Format for the date can be altered to suit my needs
-   
+    // Save user input when the save button is clicked
     $(".saveBtn").on("click", function() {
-        // Find the associated time-block ID
         const timeBlockID = $(this).closest(".time-block").attr("id");
-        // Get the user's input for this time-block
         const userInput = $(this).siblings(".description").val();
-        // Save the input to local storage with the time-block ID as the key
         localStorage.setItem(timeBlockID, userInput);
     });
 
-    // To display the users inputs in each time-block with saved text
+    // Retrieve and display user inputs from local storage
     $(".time-block").each(function() {
         const timeBlockID = $(this).attr("id");
         const storedInput = localStorage.getItem(timeBlockID);
@@ -26,15 +21,20 @@ $(function () {
             $(this).find(".description").val(storedInput);
         }
     });
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+
+    function updateTimeBlockClasses() {
+        $(".time-block").each(function() {
+            const timeBlockID = $(this).attr("id");
+            const timeBlockHour = parseInt(timeBlockID.split("-")[1]);
+            const currentHour = currentDay.hour()
+
+            if (currentHour === timeBlockHour) {
+                $(this).removeClass("past future").addClass("present");
+            } else if (currentHour > timeBlockHour) {
+                $(this).removeClass("future present").addClass("past");
+            } else {
+                $(this).removeClass("past present").addClass("future");
+            }
+        });
+    }
+});
